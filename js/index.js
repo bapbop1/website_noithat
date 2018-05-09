8f60744ef673e160c3ec905a6c448ca0bb59cacf
bapbop1 <hungnx96@gmail.com>
2018-05-09 22:20:07 +0700

    update

---
+++ js/index.js
@@ -0,0 +1,30 @@
+(function(){
+  $('.carousel-showmanymoveone .item').each(function(){
+    var itemToClone = $(this);
+
+    for (var i=1;i<4;i++) {
+      itemToClone = itemToClone.next();
+
+      // wrap around if at end of item collection
+      if (!itemToClone.length) {
+        itemToClone = $(this).siblings(':first');
+      }
+
+      // grab item, clone, add marker class, add to collection
+      itemToClone.children(':first-child').clone()
+        .addClass("cloneditem-"+(i))
+        .appendTo($(this));
+    }
+  });
+}());
+
+
+
+
+$(window).load(function() {
+ // executes when complete page is fully loaded, including all frames, objects and images
+console.log("window is loaded");
+  
+  
+// window load  
+});
